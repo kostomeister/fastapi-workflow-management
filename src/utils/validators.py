2b39362b498f46d0a0194b4b41f54924c16fa6eb
Kostia <kostia3507@gmail.com>
2024-03-11 04:09:52 +0200

    added decorator for validation

---
+++ src/utils/validators.py
@@ -0,0 +1,18 @@
+import functools
+
+from fastapi import HTTPException
+from sqlalchemy import exc
+
+
+def no_result_found_handler():
+    def wrapper(func):
+        @functools.wraps(func)
+        async def wrapped(*args):
+            try:
+                return await func(*args)
+            except exc.NoResultFound:
+                raise HTTPException(status_code=404, detail="No such element!")
+
+        return wrapped
+
+    return wrapper
