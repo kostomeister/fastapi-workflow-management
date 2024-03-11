bf2cee845489ec1d48571c9347d3bedc95b29590
Kostia <kostia3507@gmail.com>
2024-03-11 12:54:55 +0200

    added sync validator

--- src/utils/validators.py
+++ src/utils/validators.py
@@ -18,13 +18,22 @@ def no_result_found_handler():
     return wrapper
 
 
-def value_error_handler():
+def async_value_error_handler():
     def wrapper(func):
         @functools.wraps(func)
-        async def wrapped(*args):
+        async def wrapped(*args, **kwargs):
             try:
-                return await func(*args)
+                return await func(*args, **kwargs)
             except ValueError as e:
                 raise HTTPException(status_code=404, detail=str(e))
         return wrapped
+    return wrapper
+
+
+def sync_value_error_handler(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError as e:
+            raise HTTPException(status_code=404, detail=str(e))
     return wrapper
