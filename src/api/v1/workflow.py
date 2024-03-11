14c6162dba3ca923dd6d03ecd9e78474d8ff7b6b
Kostia <kostia3507@gmail.com>
2024-03-11 12:42:33 +0200

    changed name of endpoint

--- src/api/v1/workflow.py
+++ src/api/v1/workflow.py
@@ -16,7 +16,7 @@ async def get_workflows(
 
 
 @workflow_router.get("/workflows")
-async def get_workflows(
+async def create_workflows(
     workflow_service: WorkflowService = Depends(get_workflow_service),
 ):
     return await workflow_service.get_all_workflows()
